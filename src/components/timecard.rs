9ddc09fd5fb9820104863e6012a53a3e82446ce7
Matthew Bastow <matthewbastow93@gmail.com>
2025-12-15 14:54:02 +0000

    removed ping from timecard

--- src/components/timecard.rs
+++ src/components/timecard.rs
@@ -1,11 +1,7 @@
 use leptos::prelude::*;
 
 #[component]
-pub fn Timecard(
-    children: Children,
-    #[prop(optional)] large: bool,
-    #[prop(optional)] ping: bool,
-) -> impl IntoView {
+pub fn Timecard(children: Children, #[prop(optional)] large: bool) -> impl IntoView {
     let is_highlighted = RwSignal::new(large);
 
     view! {
@@ -16,14 +12,6 @@ pub fn Timecard(
                 rounded-2xl p-8 content-center relative snap-center z-1 transition
             "
         >
-            // add a pinging dot if this is the current time
-            {ping.then(||view! {
-                <span class="absolute top-2 right-2 flex size-3">
-                    <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-teal-400 opacity-75"></span>
-                    <span class="relative inline-flex size-3 rounded-full bg-teal-500"></span>
-                </span>
-            })}
-
             <div class="p-6 flex-none text-center">
                 {children()}
             </div>
